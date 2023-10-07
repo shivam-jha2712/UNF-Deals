271f35d26872cc0cd0eefa14668ba732d5d25dc3
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-10-07 15:51:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+import Card from './components/Card/Card'
+import Footer from './components/Footer/Footer'
+
+
+function App() {
+
+  return (
+    <div className="page">
+      <Header />
+      <Card />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react'
+
+const card = () => {
+    return (
+        <Fragment></Fragment>
+    )
+}
+
+export default card
